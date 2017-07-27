ba856aa95761689fbf7d90161bf86a805a9403e1
Richard Gan <richard@designedbymajor.ca>
2017-07-27 01:09:15 -0600

    Initial commit
    add the owner subset of commands to the bot
    + eval command

---
+++ commands/owner.js
@@ -0,0 +1,28 @@
+module.exports = (client) => {
+  // eval command
+  client.registerCommand("eval",
+  // function of command
+  (msg, args) => {
+    try {
+      let evaled = eval(msg.content.slice(msg.prefix.length + 5));
+      if (typeof evaled !== "string") evaled = require("util").inspect(evaled);
+      return `\`\`\`x1\n${cleanText(evaled)}\n\`\`\``;
+    } catch (err) {
+      return `Error: ${cleanText(err)}`
+    }
+  },
+  // command options
+  {
+    description: "Runs arbitrary JavaScript. Oh, and *you* can't run it.",
+    fullDescription: "Runs arbitrary JavaScript. Oh, and *you* can't run it.",
+    usage: "<Javascript>",
+    requirements: {
+      userIDs: [ "97198953430257664" ],
+    }
+  });
+}
+
+function cleanText(text) {
+  if (typeof text === "string") return text.replace(/`/g, "`\u200b").replace(/@/g, "@\u200b");
+  else return text;
+}
